791ef90b2ed4a12477b07af40363879f8cebbd9b
Evynith <evynith@gmail.com>
2022-06-28 15:36:42 -0300

    se agrega nav y footer

--- netflics/src/app/component/nav/nav.component.ts
+++ netflics/src/app/component/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  // @ViewChild('navegador')
+  // nav!: HTMLElement;
 
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    let nav = document.querySelector('nav') as HTMLElement;
+
+      if (window.scrollY > nav.clientHeight) {
+        nav.classList.add('navbar-inverse');
+      } else {
+        nav.classList.remove('navbar-inverse');
+      }
+  }
+
+
+
+  urlAvatar : string = "assets/extra/Avatar-de-perfil-de-Mirabel-Encanto-agregado-a-Disney.png";
   constructor() { }
 
   ngOnInit(): void {
   }
+
+ 
 
 }
