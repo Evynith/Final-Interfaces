50f776823dc7981ab08ffc0e5a7eb8de8b0b02ea
Evynith <cvega@alumnos.exa.unicen.edu.ar>
2022-06-29 22:28:38 -0300

    animacion home externo

--- netflics/src/app/page/home-external/home-external.component.ts
+++ netflics/src/app/page/home-external/home-external.component.ts
@@ -1,4 +1,5 @@
-import {Component, HostListener, Input, OnInit } from '@angular/core';
+import {AfterViewInit, Component, HostListener, Input, OnInit, ViewChild } from '@angular/core';
+import { timeout } from 'rxjs';
 import { ButtonComponent } from 'src/app/component/button/button.component';
 
 @Component({
@@ -6,17 +7,24 @@ import { ButtonComponent } from 'src/app/component/button/button.component';
   templateUrl: './home-external.component.html',
   styleUrls: ['./home-external.component.scss']
 })
-export class HomeExternalComponent implements OnInit {
+export class HomeExternalComponent implements OnInit, AfterViewInit {
 
   constructor() { }
+  //@ViewChild('menuHeader') menuHeader?: HTMLElement;
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() : void {
+  }
+  lugar : number = 1;
+  modalCrearCuenta : boolean = false;
+  lugarHeader : number = window.innerWidth;
+
   acordeon(acordeonElement : HTMLElement, element: ButtonComponent) {
     element.action();
     let acordeonElements = document.querySelectorAll('.box-acordeon > div');
-    acordeonElements.forEach(element => 
+    acordeonElements.forEach(element =>
       {
         if (element != acordeonElement)
            element.classList.add("hidden");
@@ -25,5 +33,64 @@ export class HomeExternalComponent implements OnInit {
     acordeonElement.classList.toggle("hidden");
   }
 
+  moverHeaderIn(){
+    //6rem = 96px
+    let menuHeader =  document.querySelector<HTMLElement>('.menuHeader');
+    if ( menuHeader!.getBoundingClientRect().width -  (window.innerWidth - (450 + 96)) >=3){
+     // console.log(menuHeader?.getBoundingClientRect().width,  (window.innerWidth - (450 + 96)) );
+      let animacion = menuHeader?.animate([
+        // w: 450 + 3rem de padding
+        { width: '100vw' },
+        { width: 'calc(100vw - (450px + 6rem))' }
+      ], {
+        // opciones de sincronización
+        duration: 1000,
+        iterations: 1
+      });
+
+      animacion?.finished.then(()=>{ menuHeader!.style.width = "calc(100vw - (450px + 6rem))";});
+
+    }
+  }
+
+  moverHeaderOut(){
+  //6rem = 96px
+  let menuHeader =  document.querySelector<HTMLElement>('.menuHeader');
+  //console.log( menuHeader!.getBoundingClientRect().width ,   (window.innerWidth - (450 + 96 - 3))   );
+  if ( menuHeader!.getBoundingClientRect().width <=   (window.innerWidth - (450 + 96 - 3)) ){
+    let animacion = menuHeader?.animate([
+      // w: 450 + 3rem de padding
+      { width: 'calc(100vw - (450px + 6rem))' },
+      { width: '100vw' }
+    ], {
+      // opciones de sincronización
+      duration: 1000,
+      iterations: 1
+    });
+
+    animacion?.finished.then(()=>{ menuHeader!.style.width = "100vw";});
+
+  }
+  }
+
+  cerrarModalCC(){
+    this.modalCrearCuenta = false;
+  }
+
+  cerrarModalForm(modal : HTMLElement) {
+   let animacion = modal.animate([
+    // fotogramas clave
+    { right: '3vw' },
+    { right: '-50vw' }
+  ], {
+    // opciones de sincronización
+    duration: 1000,
+    iterations: 1
+  });
+  this.moverHeaderOut();
+
+  animacion.finished.then(()=>{this.cerrarModalCC();});
+
+  }
 
 }
--- netflics/src/app/service/login.service.ts
+++ netflics/src/app/service/login.service.ts
@@ -4,14 +4,14 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class LoginService {
-  
-  private static _login: boolean = true;
+
+  private static _login: boolean = false;
   constructor() { }
-  
+
   static saveLogin(log: boolean){
     this._login = log;
   }
-  
+
   static islogin() {
     return this._login;
   }
